f6dfcece77e7bee2183752cb3dee99ebac27e9b7
mkuranda <kuranda.marek@gmail.com>
2024-04-06 19:10:17 +0200

    topbar

--- src/components/common/top-bar.tsx
+++ src/components/common/top-bar.tsx
@@ -1,5 +1,20 @@
+import styles from '@/styles/components/common/header.module.scss';
+
 export function TopBar() {
     return (
-        <>X</>
+        <header className={styles['header']}>
+            <span></span>
+            <div className={styles['header__search-container']}>
+                <div className={styles['header__search']}>Search</div>
+                <div className={styles['header__search-menu-container']}>
+                    <ul>
+                        <li>Menu item 1</li>
+                        <li>Menu item 1</li>
+                        <li>Menu item 1</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles['header__user-container']}>Y</div>
+        </header>
     );
 }
