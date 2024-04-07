3fd161eae0f5b56d6f349d61adbfea334bd52832
mkuranda <kuranda.marek@gmail.com>
2024-04-07 17:25:11 +0200

    break line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { TopBar } from '@/components/common/top-bar';
 import { Footer } from '@/components/common/footer';
 import { WelcomeFragment } from '@/app/welcome-fragment';
 import { BrowseProducts } from '@/app/browse-products';
+import { BreakLine } from '@/components/common/break-line';
 
 export default function MainPage() {
     return (
@@ -11,7 +12,7 @@ export default function MainPage() {
                 <WelcomeFragment />
             </div>
             <BrowseProducts />
-            <>TODO LINE</>
+            <BreakLine />
             <div>TODO 3 Products</div>
             <Footer />
         </main>
