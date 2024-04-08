8e8a6a64241ac492a0577db1051702634880c729
PJWORKs <pjan319@gmail.com>
2024-04-08 19:18:31 +0200

    add fetch function to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+import React, { useState } from 'react';
 import { TopBar } from '@/components/common/top-bar';
 import styles from '@/styles/app/login/page.module.scss';
 
 export default function LoginPage() {
+    const [loginValue, setLoginValue] = useState<string>('');
+    const [passValue, setPassValue] = useState<string>('');
+    const userData = {
+        username: '',
+        password: ''
+    };
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (loginValue.trim() === '' || passValue.trim() === '') {
+            document.write('Submitted value:', loginValue, passValue);
+        } else {
+            userData.username = loginValue;
+            userData.password = passValue;
+            fetchUserLogIn();
+            event.preventDefault();
+            //document.write('Submitted value:', loginValue, passValue);
+        }
+
+    };
+    const handleLoginChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginValue(event.target.value);
+    };
+    const handlePassChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassValue(event.target.value);
+    };
+
     return (
         <>
             <TopBar />
@@ -10,15 +37,15 @@ export default function LoginPage() {
                     <div className={styles['login-page__leftbox']}>
                     </div>
                     <div className={styles['login-page__rightbox']}>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <h4>System <span>lojalnościowy</span></h4>
                             <p>Witaj ponownie! Zaloguj się aby poznać nowości w naszej ofercie:</p>
                             <div className={styles['login-page__floating-labels']}>
-                                <input placeholder="Login" type="text" name="login" />
+                                <input placeholder="Login" type="text" name="login" value={loginValue} onChange={handleLoginChange} />
                                 <label htmlFor="login">Login:</label>
                             </div>
                             <div className={styles['login-page__floating-labels']}>
-                                <input placeholder="Password" type="password" name="password" />
+                                <input placeholder="Password" type="password" name="password" value={passValue} onChange={handlePassChange} />
                                 <label htmlFor="password">Hasło:</label>
                             </div>
                             <button type="submit">Zaloguj</button>
@@ -28,4 +55,25 @@ export default function LoginPage() {
             </main>
         </>
     );
+    async function fetchUserLogIn() {
+        try {
+            const response = await fetch('https://lojback.ne-quid-nimis.pl/api/Login/Login', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+                credentials: 'include',
+                mode: 'cors'
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to log in');
+            }
+        } catch (error) {
+            throw new Error('Failed to log in');
+            //document.write('Error logging in:', error);
+        }
+    }
 }
