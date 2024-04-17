408ce890f65b3fd82f2586274a624a6d901b8ab0
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2024-04-17 12:34:10 +0200

    [0.3.0] Main page box (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,18 @@ import { TopBar } from '@/components/common/top-bar';
 import { Footer } from '@/components/common/footer';
 import { WelcomeFragment } from '@/app/welcome-fragment';
 import { BrowseProducts } from '@/app/browse-products';
+import { PageBox } from '@/app/page-box';
 import styles from '@/styles/app/page.module.scss';
 
 export default function MainPage() {
     return (
         <main className={styles['main-page']}>
-            <div>
-                <TopBar />
+            <TopBar />
+            <PageBox>
                 <WelcomeFragment />
-            </div>
-            <BrowseProducts />
-            <Footer />
+                <BrowseProducts />
+                <Footer />
+            </PageBox>
         </main>
     );
 }
