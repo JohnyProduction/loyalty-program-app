5093b92a23d20f757cf2c3f3f3a056bc3f73c63d
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2024-04-17 15:59:07 +0200

    [0.4.0] Added `not-found` page (#4)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.0] - 2024-04-17
+### Added
+- [Marek Kurańda](https://github.com/mjkuranda): Not found page.
+
 ## [0.3.0] - 2024-04-17
 ### Added
 - [Marek Kurańda](https://github.com/mjkuranda): `PageBox` component to insert horizontal margin for page.
---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import styles from '@/styles/app/not-found.module.scss';
+import { Button } from '@/components/common/buttons/button';
+
+export default function NotFoundPage() {
+    return (
+        <div className={styles['not-found-page']}>
+            <img src="/pages/not-found.png" alt="Not found image" />
+            <div className={styles['not-found-page__navigation']}>
+                <p className={styles['not-found-page__text']}>Opps! Page not found!</p>
+                <Button label={'Back to home'} link="/" size="small" />
+            </div>
+        </div>
+    );
+}
