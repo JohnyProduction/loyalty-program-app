855e18d431e4a62c3b3061b0ecd2b4532074e252
mkuranda <kuranda.marek@gmail.com>
2024-04-17 16:41:52 +0200

    contact form us page & version

---
+++ src/app/contact-us/form-container.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/app/contact-us/page.module.scss';
+
+export function FormContainer() {
+    return (
+        <div className={styles['contact-us-page__form-container__form-container']}>
+            Form container
+        </div>
+    );
+}
