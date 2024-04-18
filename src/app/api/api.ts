d39c4e4a11b3d829ca1151e7e6b43043a5c16ee4
kmlwoj <wujekkama@gmail.com>
2024-04-18 23:01:21 +0200

    Added user endpoints

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,4 +1,5 @@
 import * as LoginTypes from '../../types/login-types';
+import * as UserTypes from '../../types/user-types';
 import { apiErrorFactory } from './api-error-factory';
 
 const API_BASE_URL = 'https://lojback.ne-quid-nimis.pl/api';
@@ -65,6 +66,115 @@ export const Login = {
             method: 'GET',
             credentials: 'include',
             headers: { 'accept': '*/*' }
+        })
+            .then(async (response) => {
+                if (!response.ok)
+                    throw await apiErrorFactory(response);
+
+                return response;
+            });
+
+        return res.text();
+    }
+};
+export const User = {
+    /** Registers new user (with manager privileges) */
+    async addUserEnd(user: UserTypes.UserModel): Promise<string> {
+        const res = await fetch(`${API_BASE_URL}/User/AddUser`, {
+            mode: 'cors',
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'accept': '*/*', 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+        })
+            .then(async (response) => {
+                if (!response.ok)
+                    throw await apiErrorFactory(response);
+
+                return response;
+            });
+
+        return res.text();
+    },
+    /** Retrieves list of users from a given organization */
+    async getUsersEnd(organization?: string): Promise<UserTypes.UserDbModel[]> {
+        const res = await fetch(`${API_BASE_URL}/User/GetUsers${organization !== undefined ? '?organization=' + organization : '' }`, {
+            mode: 'cors',
+            method: 'GET',
+            credentials: 'include',
+            headers: { 'accept': '*/*' }
+        })
+            .then(async (response) => {
+                if (!response.ok)
+                    throw await apiErrorFactory(response);
+
+                return response;
+            });
+
+        return res.json();
+    },
+    /** Edits email of a given user */
+    async editUserMailEnd(user: UserTypes.UserDbModel): Promise<string> {
+        const res = await fetch(`${API_BASE_URL}/User/EditUserMail`, {
+            mode: 'cors',
+            method: 'PUT',
+            credentials: 'include',
+            body: JSON.stringify(user),
+            headers: { 'accept': '*/*', 'Content-Type': 'application/json' }
+        })
+            .then(async (response) => {
+                if (!response.ok)
+                    throw await apiErrorFactory(response);
+
+                return response;
+            });
+
+        return res.text();
+    },
+    /** Deletes a given user */
+    async deleteUserEnd(user: UserTypes.UserDbModel): Promise<string> {
+        const res = await fetch(`${API_BASE_URL}/User/DeleteUser`, {
+            mode: 'cors',
+            method: 'DELETE',
+            credentials: 'include',
+            body: JSON.stringify(user),
+            headers: { 'accept': '*/*', 'Content-Type': 'application/json' }
+        })
+            .then(async (response) => {
+                if (!response.ok)
+                    throw await apiErrorFactory(response);
+
+                return response;
+            });
+
+        return res.text();
+    },
+    /** Changes targeted user's credits */
+    async changeCreditsEnd(login: string, amount: number): Promise<string> {
+        const res = await fetch(`${API_BASE_URL}/User/ChangeCredits/${amount}`, {
+            mode: 'cors',
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(login),
+            headers: { 'accept': '*/*', 'Content-Type': 'application/json' }
+        })
+            .then(async (response) => {
+                if (!response.ok)
+                    throw await apiErrorFactory(response);
+
+                return response;
+            });
+
+        return res.text();
+    },
+    /** Changes password of currently logged in user */
+    async setPasswordEnd(password: string): Promise<string> {
+        const res = await fetch(`${API_BASE_URL}/User/SetPassword`, {
+            mode: 'cors',
+            method: 'PUT',
+            credentials: 'include',
+            body: JSON.stringify(password),
+            headers: { 'accept': '*/*', 'Content-Type': 'application/json' }
         })
             .then(async (response) => {
                 if (!response.ok)
---
+++ src/types/user-types.ts
@@ -0,0 +1,15 @@
+import { AccountTypes } from './login-types';
+
+export type UserModel = {
+    username: string,
+    password: string,
+    accountType: AccountTypes,
+    email: string
+};
+export type UserDbModel = {
+    login: string,
+    password: string,
+    type: AccountTypes,
+    credits: number,
+    latestUpdate: Date //might need a change to string
+};
