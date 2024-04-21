b39b2920426b23bca5e08e2b1a691d8936ad7cf0
PJWORKs <pjan319@gmail.com>
2024-04-22 00:02:10 +0200

    create files for addUser

---
+++ src/app/addUser/addUser.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TopBar } from '@/components/common/top-bar';
+import styles from '@/styles/app/addUser/page.module.scss';
+export default function LoginPage() {
+    return (
+        <>
+            <TopBar />
+            <main className={styles['main-page']}>
+            </main>
+        </>
+    );
+}
