d27c6f3d175768f21b7b6b788fd4565ce5b416bc
PJWORKs <pjan319@gmail.com>
2024-04-22 15:32:35 +0200

    Completed form without css

---
+++ src/app/add-user/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import React, { useState } from 'react';
+import { TopBar } from '@/components/common/top-bar';
+import styles from '@/styles/app/add-user/page.module.scss';
+import { InputString } from '@/components/common/inputs/input-string';
+import { User } from '../api/api';
+import { AccountTypes } from '@/types/login-types';
+export default function AddUserPage() {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const newUser={
+        username: '',
+        password: '',
+        accountType: AccountTypes.ADMINISTRATOR,
+        email: ''
+    };
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (username.trim() !== '' || password.trim() !== '' || email.trim() !== '') {
+            newUser.username = username;
+            newUser.password = password;
+            newUser.email = email;
+            User.addUserEnd(newUser);
+            event.preventDefault();
+        }
+
+    };
+
+    return (
+        <>
+            <TopBar />
+            <main className={styles['main-page']}>
+                <div className={styles['addUser-page__container']}>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <h3>Panel dodawania menadżerów</h3>
+                            <InputString label='Nazwa użytkownika' name='username' value={username} onChange={handleUsernameChange} />
+                            <InputString label='Hasło użytkownika' name='password' value={password} onChange={handlePasswordChange} />
+                            <InputString label='Email użytkownika' name='email' value={email} onChange={handleEmailChange} />
+                        </div>
+                        <button type="submit">Dodaj</button>
+                    </form>
+                </div>
+            </main>
+        </>
+    );
+}
