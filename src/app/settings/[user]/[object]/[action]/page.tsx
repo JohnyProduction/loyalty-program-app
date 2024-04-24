a5e01a444e65192fbd8618aa1428d187d0cdbaed
mkuranda <kuranda.marek@gmail.com>
2024-04-24 13:03:54 +0200

    struct

--- src/app/settings/[user]/[object]/[action]/page.tsx
+++ src/app/settings/[user]/[object]/[action]/page.tsx
@@ -1,4 +1,6 @@
 import styles from '@/styles/app/settings/[user]/[object]/[action]/page.module.scss';
+import { TopBar } from '@/components/common/top-bar';
+import { Footer } from '@/components/common/footer';
 
 interface SettingsPageProps {
     params: Record<string, any>;
@@ -11,7 +13,12 @@ export default function SettingsPage({ params }: SettingsPageProps) {
 
     return (
         <main className={styles['settings-page']}>
-            <>Settings</>
+            <TopBar />
+            <div className={styles['content-box']}>
+                <div className={styles['link-container']}></div>
+                <div className={styles['creator-container']}></div>
+            </div>
+            <Footer />
         </main>
     );
 }
