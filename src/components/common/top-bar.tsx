dbf9fab5d7d2c5610d9a235e96eb38252670a1fd
PJWORKs <pjan319@gmail.com>
2024-04-26 01:46:17 +0200

    create simple submenu for logged user

--- src/components/common/top-bar.tsx
+++ src/components/common/top-bar.tsx
@@ -1,6 +1,17 @@
+'use client';
 import styles from '@/styles/components/common/header.module.scss';
-
+import { useEffect, useState } from 'react';
+import { Login, User } from '@/app/api/api';
 export function TopBar() {
+
+    let userData;
+    let isLogged=true;
+
+    useEffect(() => {
+        //isLogged = Login.isLoggedInEnd();
+        userData = User.getCurrentUserEnd();
+    }, []);
+
     return (
         <header className={styles['header']}>
             <span></span>
@@ -17,8 +28,24 @@ export function TopBar() {
                 </div>
             </div>
             <div className={styles['header__user-container']}>
-                <p>1000 PKT</p>
-                <div className={styles['header__user-profile']}>user profile</div>
+                {isLogged ?
+                    <><p>{userData.credits} PKT</p>
+                        <div className={styles['header__user-profile']}>
+                            <ul>
+                                <li> {/*userData.login*/}</li>
+                                <li>
+                                    <ul>
+                                        <li>Settings</li>
+                                        <span></span>
+                                        <li>Logout</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </>
+                    :
+                    <> <div className={styles['header__user-profile']}>Sign in</div></>
+                }
             </div>
         </header>
     );
