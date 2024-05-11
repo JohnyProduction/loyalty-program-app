a0388a8f4e52307c6ee2f581f81c65af058c4da8
mkuranda <kuranda.marek@gmail.com>
2024-05-11 21:19:30 +0200

    styles

--- src/app/browse-product.tsx
+++ src/app/browse-product.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from '@/types/main-page-types';
 import styles from '@/styles/components/browse-product.module.scss';
+import Link from 'next/link';
 
 export interface BrowseProductProps {
     product: ProductType;
@@ -7,13 +8,15 @@ export interface BrowseProductProps {
 
 export function BrowseProduct({ product }: BrowseProductProps) {
     return (
-        <div className={styles['browse-product']}>
-            <div className={styles['browse-product__image-container']}>
-                <div className={styles['browse-product__image']}>
-                    <img src={product.imageUrl} alt={`${product.name}'s category image`} />
+        <Link href={`/categories/${product.name}`}>
+            <div className={styles['browse-product']}>
+                <div className={styles['browse-product__image-container']}>
+                    <div className={styles['browse-product__image']}>
+                        <img src={product.imageUrl} alt={`${product.name}'s category image`} />
+                    </div>
                 </div>
+                <div className={styles['browse-product__name']}>{product.name}</div>
             </div>
-            <div className={styles['browse-product__name']}>{product.name}</div>
-        </div>
+        </Link>
     );
 }
