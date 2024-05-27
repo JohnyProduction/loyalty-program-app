9ce1696d8b0b39c56aa0382dd3b20d6923383a4b
mkuranda <kuranda.marek@gmail.com>
2024-05-27 22:28:12 +0200

    submitting buying codes

--- src/app/shops/[name]/offers/[id]/product-details.tsx
+++ src/app/shops/[name]/offers/[id]/product-details.tsx
@@ -6,9 +6,13 @@ import { RectangularButton } from '@/components/common/buttons/rectangular-butto
 import Link from 'next/link';
 import { useCounter } from '@/hooks/use-counter';
 import { useGetOffer } from '@/hooks/use-get-offer';
-import { Loader } from '@/components/common/loader';
 import { useParams } from 'next/navigation';
 import { OfferImage } from '@/app/shops/[name]/offers/[id]/offer-image';
+import { useContext, useState } from 'react';
+import { ProfileContext } from '@/contexts/profile-context';
+import { Offers, Transactions } from '@/api/api';
+import { toastError, toastSuccess } from '@/utils/toast-utils';
+import { Loader } from '@/components/common/loader';
 
 interface ProductDetailsProps {
     productId: string;
@@ -17,16 +21,55 @@ interface ProductDetailsProps {
 export function ProductDetails({ productId }: ProductDetailsProps) {
     const { name: organization, id: offerId } = useParams();
     const counterProps = useCounter(0);
+    const { profile } = useContext(ProfileContext);
     const { offer, isLoading, image, isLoadingImage } = useGetOffer(organization.toString(), Number(offerId));
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-    const renderTotalPrice = (price: number) => {
-        return `${price * counterProps.count} PKT`;
+    const renderTotalPrice = (price?: number) => {
+        return `${(price ?? 0) * counterProps.count} PKT`;
+    };
+
+    const onBuy = async () => {
+        setIsSubmitting(true);
+
+        try {
+            const { checkOfferCodesEnd } = Offers;
+            const id = Number(offerId);
+
+            const codes = await checkOfferCodesEnd(id);
+            const availableCodes = codes.filter(code => !code.isUsed);
+
+            if (counterProps.count > availableCodes.length) {
+                toastError('Insufficient number of available codes for this offer.');
+
+                return;
+            }
+
+            const totalCost = (offer?.price ?? 0) * counterProps.count;
+
+            if (profile?.credits ?? 0 < totalCost) {
+                toastError('Insufficient amount of credits.');
+
+                return;
+            }
+
+            const { buyCode } = Transactions;
+
+            for (let i = 0; i < counterProps.count; i++) {
+                await buyCode(id);
+            }
+
+            toastSuccess(`You have successfully bought ${counterProps.count} codes.`);
+        } finally {
+            setIsSubmitting(false);
+        }
     };
 
     return (
         <div className={styles['product-details']}>
+            {isSubmitting && <Loader isAbsolute={true} />}
             {isLoading
-                ? <Loader isAbsolute={true} />
+                ? <></>
                 : (
                     <>
                         <OfferImage image={image} isLoadingImage={isLoadingImage} />
@@ -41,14 +84,6 @@ export function ProductDetails({ productId }: ProductDetailsProps) {
                                 </div>
                                 <div className={styles['details-information__container']}>
                                     <div>
-                                        {/*<div className={styles['details-information__information-box']}>*/}
-                                        {/*    <img src="/pages/products/location-pin.png" alt="Location pin" />*/}
-                                        {/*    <p>sklep internetowy</p>*/}
-                                        {/*</div>*/}
-                                        {/*<div className={styles['details-information__information-box']}>*/}
-                                        {/*    <img src="/pages/products/calendar.png" alt="Calendar" />*/}
-                                        {/*    <p>ważna do 25.03.2025</p>*/}
-                                        {/*</div>*/}
                                         <div className={styles['details-information__information-box']}>
                                             <img src="/pages/products/web.png" alt="Web" />
                                             <p><Link href={`/shops/${organization}/offers`}>More offers</Link></p>
@@ -58,10 +93,10 @@ export function ProductDetails({ productId }: ProductDetailsProps) {
                             </div>
                             <hr />
                             <div className={styles['details-transaction']}>
-                                <p className={styles['product-cost']}>{renderTotalPrice(offer!.price)}</p>
+                                <p className={styles['product-cost']}>{renderTotalPrice(offer?.price)}</p>
                                 <div className={styles['transaction-box']}>
                                     <InputCounter {...counterProps} />
-                                    <RectangularButton label={'Buy now'} link={'/'} size="small" bgcolor="orange" />
+                                    <RectangularButton label={'Buy now'} link={''} size="small" bgcolor="orange" disabled={(offer?.price ?? 1) * counterProps.count > (profile?.credits ?? 1)} onClick={onBuy} />
                                 </div>
                             </div>
                         </div>
